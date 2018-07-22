10726162c1828dae27b89631dfc87676d1d71d6c
garretbs <garretbs@gmail.com>
2018-07-22 11:33:47 -0400

    Added support for Twitter profile images, fixed Instagram regex bug

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ media.tumblr.com, pbs.twimg.com, Instagram posts
 ## Usage
 Plug and play. Automatic for Tumblr and Twitter. For Instagram, you must be on a post's page. Right clicking will automatically load the higher res image.
 ## Todo
-* Figure out why the Instagram regex does not work
 * Make Instagram image fetching better
+* Reddit support, others?
--- amsi.js
+++ amsi.js
@@ -5,30 +5,36 @@ var pageName = urlSplit.pop()
 //var fileType = pageSplit[pageSplit.length-1]
 
 //Get max res Tumblr image (1280 as of version 1.0)
+//console.log = function(){}
 if(/tumblr\.com/i.test(document.domain)){
-	//console.log("Tumblr!")
+	console.log("Tumblr!")
 	if((!/_1280\.(png|gif|jpg)$/.test(pageName))){ //so we don't constantly reload
 		document.location.href = urlName.replace(/_\d+\.(png|gif|jpg)$/, "_1280.$1")
 	}
 }
-//Get max res twimg image (append:orig to image url)
+//Get max res twimg image
 else if(/pbs\.twimg\.com/i.test(document.domain)){
-	//console.log("Twimg!")
-	//console.log(pageName)
-	if((!/:orig$/i.test(pageName))){
+	console.log("Twimg!")
+	if(/profile_images/.test(urlName)){ //if profile image
+		console.log("Profile image")
+		if(/_\d+x\d+/.test(pageName)){
+			urlName = urlName.replace(/_\d+x\d+/i, "")
+			document.location.href = urlName
+		}
+	}else if((!/:orig$/i.test(pageName))){ //if not already an :orig image
+		console.log("Regular image")
 		urlName = urlName.replace(/:large$/i, "")
 		urlName += ":orig"
 		document.location.href = urlName
+	}else{
+		console.log("Unknown twimg image type?")
 	}
-}/*else if(/instagram\.com/i.test(pageName)){
+}
+//Get max res instagram image when right click
+else if(/instagram\.com/i.test(urlName)){
 	console.log("Instagram!")
 	//small image starts with "display_resources":[{"src":"small_image_url",
 	//large image is <meta property="og:image" content="large_image_url" />
-}else{
-	console.log("Unknown site: " + document.domain)
-}*/
-//Get max res Instagram image (found in HTML data of post page)
-else{
 	//console.log("Instagram!")
 	document.addEventListener("contextmenu", function(e){
 		document.location.href = document.querySelector('meta[property="og:image"]').content
@@ -36,4 +42,6 @@ else{
 	//window.open(largePicUrl, '_blank')
 	//var smallPicData
 	//console.log(largePicUrl)
+}else{
+	console.log("Unknown site: " + document.domain)
 }
