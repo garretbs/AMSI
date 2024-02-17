b7869e380205fb1d83276e491c5e4800d8a28de7
Eblo <7004497+Eblo@users.noreply.github.com>
2024-02-17 10:58:41 -0500

    Un-break Instagram again
    The changes to how this work are big enough to warrant a minor version bump

--- instagram.js
+++ instagram.js
@@ -1,48 +1,38 @@
-console.log("Instagram!");
+console.log('Instagram!');
 let mediaContainer = null;
-let mediaObject = null;
+let mediaObjects = null;
 const downloadButton = document.createElement('button');
-downloadButton.innerHTML = "Download";
+downloadButton.innerHTML = 'Download';
 downloadButton.onclick = function() {
-	// Go through children until img/video found
-	let results = [];
-	const getMediaElement = function(node) {
-		if (node.nodeName === 'IMG' || node.nodeName === 'VIDEO') {
-			results.push(node);
-			return;
-		}
-		for(let i = 0; i < node.children.length; i++) {
-			getMediaElement(node.children[i]);
-		}
-	};
-	getMediaElement(mediaContainer);
 
-	// There can be up to three media found: previous, current, next.		
-	// If there is no back button, take the first
-	// Else if there is no next button, take the last
-	// Otherwise, take the middle
-	let resultsIndex = -1;
-	const backButtonElement = document.querySelector('[aria-label="Go Back"]');
-	const nextButtonElement = document.querySelector('[aria-label="Next"]');
-	if(backButtonElement) {
-		resultsIndex = 1;
-	} else {
-		resultsIndex = 0;
-	}
+	let carousel = null;
+	mediaObjects = [];
+	let objects = document.querySelectorAll('[data-content-len]');
+	objects.forEach((object) => mediaObjects.push(JSON.parse(object.textContent)));
 
-	const currentMedia = results[resultsIndex];
-	if(currentMedia.nodeName === 'VIDEO') {
-		// I don't know of a great way to know which video URLs map to the current media, so just open all of them.
-		mediaObject.forEach((object) => {
-			const video = object?.require?.[0]?.[3]?.[0]?.__bbox?.require?.[0]?.[3]?.[1]?.__bbox?.result?.data;
-			if(!video) return;
+	mediaObjects.forEach((object) => {
+		if(carousel) return;
 
-			const url = video.xdt_api__v1__media__shortcode__web_info?.items[0].video_versions[0].url;
-			if (url) open(url, '_blank');
+		// TODO: This logic seems sound, but it seems like navigating to other pages in the same tab will result in
+		// stale data here...
+		const media = object?.require?.[0]?.[3]?.[0]?.__bbox?.require?.[0]?.[3]?.[1]?.__bbox.result?.data;
+		carousel = media?.xdt_api__v1__media__shortcode__web_info?.items[0].carousel_media;
+		if(!carousel) return;
+
+		carousel.forEach((displayItem) => {
+			const videos = displayItem.video_versions;
+			if(videos) {
+				open(videos[0].url, '_blank');
+				return;
+			}
+
+			const images = displayItem.image_versions2;
+			if(images) {
+				open(images.candidates[0].url, '_blank');
+				return;
+			}
 		});
-	} else {
-		open(currentMedia.src, '_blank');
-	}
+	});
 };
 
 let currentUrl = null;
@@ -51,9 +41,6 @@ function checkForMedia() {
 	// If there is no media container div, or we're still on the same page, don't add the download button
 	if(!mediaContainer || window.location.href === currentUrl) return;
 
-	let objects = document.querySelectorAll('[type="application/json"]')
-	mediaObject = []
-	objects.forEach((object) => mediaObject.push(JSON.parse(object.textContent)));
 	currentUrl = window.location.href;
 	mediaContainer.append(downloadButton);
 }
